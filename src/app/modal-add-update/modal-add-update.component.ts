05ef571b4bb1e7451da0796af3acd5b8c237a3c1
erickedrm <54421796+erickedrm@users.noreply.github.com>
2020-06-19 00:16:48 -0500

    E5 A13.5

--- src/app/modal-add-update/modal-add-update.component.ts
+++ src/app/modal-add-update/modal-add-update.component.ts
@@ -10,16 +10,50 @@ import { Automovil } from '../model';
 })
 export class ModalAddUpdateComponent implements OnInit {
 
-
+  aceptar:boolean = true;
   accion:String;
-
+  aniomaximo:number;
+  aniominimo:number;
   auto: Automovil = {} as Automovil;
-
+  modeloss:number[] = [];
   constructor(public activeModal:NgbActiveModal) { }
 
 
 
   ngOnInit(): void {
+   if(this.accion == 'Editar'){
+     this.aniominimo = this.auto.modelos[0];
+     this.aniomaximo = this.auto.modelos[this.auto.modelos.length-1]
+   }else{
+     this.aniominimo = 2000;
+     this.aniomaximo = 2020;
+   }
+
   }
+  
+  onSubmit() {
+    console.log('onSubmit()')
+  
+    for (let index = this.aniominimo; index <= this.aniomaximo ; index++) {
+      //const element = array[index];
+      this.modeloss?.push(index);
+      
+      
+    }
+    
+    
+    this.auto.modelos = this.modeloss;
+
+    this.activeModal.close(this.auto);
+
+  }
+
+  anios(){
+    if(this.aniomaximo < this.aniominimo){
+      this.aniomaximo = this.aniominimo;
+    }
+  }
+
+  
 
 }
